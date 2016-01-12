89743269e9561c2350a6451b908d89c2c023edde
Chris Stanford <jcjs@princeton.edu>
2016-01-12 18:58:40 -0500

    suggestedfixrevert1

--- lotrdb.js
+++ lotrdb.js
@@ -490,7 +490,7 @@
     };
   });
 
-  app.factory('suggested', ['filtersettings','cardObject',function(filtersettings,cardObject){
+  app.factory('suggested', ['filtersettings','deck','cardObject',function(filtersettings,deck,cardObject){
     var suggested={};
     suggested.filtersettings = filtersettings;
     suggested.allcards = cardObject
@@ -510,6 +510,7 @@
 	    }
 	}
     };
+
 
     suggested.clear = function(card) {
 	for(var c in suggested[card.type]) {
