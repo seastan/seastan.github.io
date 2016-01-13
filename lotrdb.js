495ed2c9c6e694d644dd96dc9affa3793f79586c
Chris Stanford <jcjs@princeton.edu>
2016-01-13 08:07:20 -0500

    suggested addcuts no samename

--- lotrdb.js
+++ lotrdb.js
@@ -597,7 +597,7 @@
 	for(var k in filtersettings.pack)
 	    if(filtersettings.pack[k]==card.exp) properexp=1;
 
-	if (samename) return;
+	//	if (samename) return;
 	if (!propersphere) return;
 	if (!properexp) return;
 	suggested[card.type].push(card);
